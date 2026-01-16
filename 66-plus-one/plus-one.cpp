960ef39a5c253d455362c99b6e0daa1960a7c2b6
pradheepa-jshree <pradheepajayashree@gmail.com>
2026-01-16 14:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (15.79%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
 
-        // Traverse from last digit to first
+       
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;   // Just add 1
                 return digits;
             }
-            digits[i] = 0;     // If it's 9, make it 0 and carry continues
+            digits[i] = 0;     
         }
 
-        // If all digits were 9, we need an extra digit at front
+       
         digits.insert(digits.begin(), 1);
         return digits;
     }
